c1948d917d7ed163bb481602c8e589e79032b920
baronadar463 <baronadar463@gmail.com>
2025-03-13 20:36:53 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Project123456 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int result = random.nextInt(10);
+        System.out.println("The generated number is: " + result);
+    }
+}
