cfd1c98927cfbf2577a8a322bf7d74573d87b26b
baronadar463 <baronadar463@gmail.com>
2025-03-19 05:24:45 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Project123456 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(10);
+        System.out.println("The random number is: " + randomNumber);
+    }
+}
