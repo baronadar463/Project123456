0aaec16dc4b6087dc101bb268a263e52954c77ba
baronadar463 <baronadar463@gmail.com>
2025-04-27 02:13:28 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,10 @@
+public class Project123456 {
+    public static void main(String[] args) {
+        // Example of using loops in Java
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Looping number: " + i);
+            i++;
+        }
+    }
+}
